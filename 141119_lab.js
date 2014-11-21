3299277d900c9ab6b3188ca3d1b671befddf306f
Joe McDonough <2jomcd@gmail.com>
2014-11-20 17:15:15 -0800

    Exercises for DOM lab

--- 141119_lab.js
+++ 141119_lab.js
@@ -1,91 +1,98 @@
-//sillySum(arr)
-//Write a function that takes an array of numbers, 
-//and returns the sum of each number multiplied by its index.
+sillySum(arr)
+Write a function that takes an array of numbers, 
+and returns the sum of each number multiplied by its index.
 
-// var prodIndex = 0;
-// var sumProdIndex = 0;
-// var inputArray = [3,4,5];
-// var l = inputArray.length;
-// var plusProdIndex = function(x) {
-// 	for (i=0;i<l;i++) {
-// 	prodIndex = (inputArray[i]*i);;
-// 	sumProdIndex += prodIndex;}
-// 	return sumProdIndex;
-// };
-// plusProdIndex(inputArray);
+var prodIndex = 0;
+var sumProdIndex = 0;
+var inputArray = [3,4,5];
+var l = inputArray.length;
 
+var plusProdIndex = function(x) {
+	for (i=0;i<l;i++) {
+	prodIndex = (inputArray[i]*i);;
+	sumProdIndex += prodIndex;}
 
-
-// numSquare(max)
-// Create a function called numSquare that 
-// will return an array of all perfect square numbers up to, but not exceeding a max number.
-
-// var perSq = [];
-// var numSquare = function(max) {
-// for (i=0;i<=max;i++) {
-// 	if (i%Math.sqrt(i)===0) {
-// 		perSq.push(i);
-// 	}
-// }
-// return perSq;
-// }
-// numSquare(max);
-
-// function isPrime(num) {
-// 	var arTp = [];
-// 	for (var i=2;i<num;i++) {
-// 		if (num%i===0) {
-// 			arTp.push(i);
-// 		}
-// 	}
-
-// 	if (arTp==0) {
-// 		return true;
-// 	}
-
-// 	else {
-// 		return false;
-// 	}
-// }
+return sumProdIndex;
+};
+plusProdIndex(inputArray);
 
 
-// primes(max)
-// Using your isPrime() function, create 
-// a function primes that will return an array of 
-// all prime numbers up to a certain amount.
+
+numSquare(max)
+Create a function called numSquare that 
+will return an array of all perfect square numbers up to, but not exceeding a max number.
+
+
+function numSquare(max) {
+	var perSq = [];
+	for (i=0;i<=max;i++) {
+		if (i%Math.sqrt(i)===0) {
+			perSq.push(i);
+	}
+}
+return perSq;
+}
+
+
+
+numSquare(10);
+
+
+function isPrime(num) {
+	if (num == 1) return false;
+	for (var i=2;i<num;i++) {
+		if (num%i===0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+primes(max)
+Using your isPrime() function, create 
+a function primes that will return an array of 
+all prime numbers up to a certain amount.
 
 //primes(max)
-// function isPrime(num) {
-// 	var arTp = [];
-// 	for (var i=2;i<num;i++) {
-// 		if (num%i===0) {
-// 			arTp.push(i);
-// 		}
-// 	}
 
-// 	if (arTp==0) {
-// 		return true;
-// 	}
+function isPrime(num) {
+	if (num == 1) return false;
+	for (var i=2;i<num;i++) {
+		if (num%i===0) {
+			return false;
+		}
+	}
+	return true;
+}
 
-// 	else {
-// 		return false;
-// 	}
-// }
-
-
-// function primes(max) {
-// primA = []		
-// 	for (var x=2; x<max; x++) {
-// 	if (isPrime(x)===true) {
-// 		primA.push(x);
-// 		}
-// 	}
-// 	return primA
-// }
+function primes(max) {
+primA = []		
+	for (var x=2; x<max; x++) {
+	if (isPrime(x)===true) {
+		primA.push(x);
+		}
+	}
+	return primA
+}
 
 
+//Write a function that takes a string that 
+//finds out how many times a character occurs. 
 
 
+function countLetters(word)  {
+	var letters = {};
+	var l = word.length;
+	for (var i=0; i<l; i++) {
+	    if (!letters[word[i]]) {
+		    letters[word[i]] = 1;
+	    } else {
+			letters[word[i]]++;
+		}
+	}
+	return letters;
+};
 
 
 
---
+++ assignments/week-1/dom-exercises/joe_mcdonough/dom.js
@@ -0,0 +1,1 @@
+document.querySelector("#hplogo").children[0].children[0].setAttribute("src", "http://scienceblogs.com/gregladen/files/2012/12/Beautifull-cat-cats-14749885-1600-1200-590x442.jpg ")
